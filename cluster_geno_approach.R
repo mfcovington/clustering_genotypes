5494a7eaa1eb3c4a19c5b343b0f7fb451bc093bb
Mike Covington <mfcovington@gmail.com>
2013-01-11 22:40:36 -0800

    fix function name style; use correct function name when reading files

--- cluster_geno_approach.R
+++ cluster_geno_approach.R
@@ -4,12 +4,12 @@ setwd("/Users/mfc/git.repos/clustering_genotypes/samples/genotyped/")
 
 # function to read multiple files at once adapted from:
 # http://stackoverflow.com/questions/2104483/how-to-read-table-multiple-files-into-a-single-table-in-r/2104532#2104532
-read.multi.tables <- function(file.names, ...) {
+ReadMultiTables <- function(file.names, ...) {
     require(plyr)
     ldply(file.names, function(fn) data.frame(Filename=fn, read.table(fn, ...)))
 }
 
-calc.score <- function(sample.df) {
+CalcScore <- function(sample.df) {
     colnames(sample.df) <- c("filename", "chr", "pos", "par1", "par2", "tot")
     sample.df <- cbind(sample.df, (sample.df$par2 - sample.df$par1) / sample.df$tot)
     colnames(sample.df)[7] <- "ratio"
@@ -28,8 +28,8 @@ GenoCor <- function(files) {
     merged <- data.frame(chr = NA, pos = NA)
     for (id in ids) {
         print(id)
-        data <- read.tables(c(list.files(pattern = id)))
-        data <- calc.score(data)
+        data <- ReadMultiTables(c(list.files(pattern = id)))
+        data <- CalcScore(data)
         merged <- merge(merged, data[, c(2, 3, 8)], by = c("pos", "chr"), all = TRUE)
         colnames(merged)[ncol(merged)] <- id
     }
